a1ae738dac8c7cc70b95cb9a953675f42e882cf7
JasicMilan <107279622+JasicMilan@users.noreply.github.com>
2022-06-14 12:57:56 +0200

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,36 +1,54 @@
-const express = require('express')
-const fileupload = require('express-fileupload')
+const express = require('express');
+const fileupload = require('express-fileupload');
+const { stringify } = require('querystring');
 
-const app = express()
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(fileupload({
     createParentPath: true
 }));
-//app.use(express.json())
-//app.use(bodyParser.json());
 
-let size = 3;
-index=[{ id: 1, name: '1.jpg'},
-        {id: 2, name: '2.jpg'},
-        {id: 3, name: '3.jpg'}]
+const index = require( __dirname + "/index.json");
 
+//Sending list of available images
 app.get('/', (req, res) => {
-    res.send(index);
+    res.json(index);
 })
 
+//Sending image
 app.get('/:id', (req, res) => {
     console.log(req.params.id);
-    res.sendFile(index[parseInt(req.params.id)].name,  { root: __dirname + "/images"})
+    res.sendFile(index[parseInt(req.params.id)].name,  { root: __dirname + "/images"});
 })
 
-app.post('/test',(req, res) => {
-    //console.log(req.body)
-    const image = req.files.myFile
-    const path = __dirname + '/images/' + image.name
+//Updating images
+app.post('/upload',(req, res) => {
+    const image = req.files.myFile;
+    const path = __dirname + '/images/' + image.name;
+    let size = +index[0].size + 1;
 
-    image.mv(path)
-    index.push({id: size + 1, name: image.name})
-    res.send(size)
+    index[0].size = size.toString();
+
+    image.mv(path);
+    index.push({id: index[0].size, name: image.name});
+
+    const fs = require("fs");
+    let indjson = fs.readFileSync("index.json","utf-8");
+
+    let ind = JSON.parse(indjson);
+
+    ind.push({id: index[0].size , name: image.name});
+    ind[0].size=index[0].size;
+
+    indjson = JSON.stringify(ind);
+
+    fs.writeFileSync("index.json",indjson,"utf-8");
 })
 
-app.listen(5000, () => console.log('Server started'))
+app.listen(5000, () => console.log('Server started'));
